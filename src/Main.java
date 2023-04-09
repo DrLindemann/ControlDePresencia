b7898253efef0e5e6fde8aa4406d6a85a3475f48
DrLindemann <69mmc69@gmail.com>
2023-04-10 01:57:52 +0200

    añadido metodo para ticar entrada

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,84 @@
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         boolean runApp = true;
         Trabajador trabajador = new Trabajador();
+        Scanner scan = new Scanner(System.in);
         List<Trabajador> listaEntradas = new ArrayList<>();
-        List<Trabajador> listaSalidas = new ArrayList<>();
+        int pass = 0000;
+        String option;
+
+        while (runApp) {
+            System.out.println("----CONTROL DE PRESENCIA----");
+            System.out.println("Elige el numero de la opcion de que desees.");
+            System.out.println("-1) Ticar Entrada");
+            System.out.println("-2) Ticar Salida");
+            System.out.println("-3) Listado de Entradas");
+            System.out.println("-4) Listado de Salidas");
+            System.out.println("-5) Guardar lista de Entradas");
+            System.out.println("-6) Guardar lista de Salidas");
+            System.out.println("-7) Salir");
+
+            option = scan.nextLine();
+
+            switch (option) {
+
+                case "1": {
+
+                }
+
+            }
+
+
+        }
+    }
+
+    public Trabajador setEntrada() {
+        boolean runIn = true;
+
+        Scanner scan = new Scanner(System.in);
+
+        LocalTime localtime = LocalTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm:ss");
+        String horaActual = localtime.format(formatter);
+
+        Trabajador trabajador = new Trabajador();
+
+        while (runIn){
+            System.out.println("Introduce numero de usuario");
+            String user = scan.nextLine();
+            System.out.println("Introduce la contraseña");
+            String pass = scan.nextLine();
+
+            if (user.equals(pass)) {
+                trabajador.setDni(user);
+                trabajador.setPass(pass);
+                trabajador.setHora(horaActual);
+                runIn = false;
+                return trabajador;
+
+            }else{
+                System.out.println("DATOS INCORRECTOS");
+                System.out.println("-1) volver a intentarlo");
+                System.out.println("-2) menu principal");
+                String opcion = scan.nextLine();
+
+                switch(opcion){
+                    case "1": runIn = true; break;
+                    case "2": runIn = false; break;
+                    default:
+                        System.out.println("Solo puedes elegir 1 o 2");
+                }
+
+
+            }
+        }
 
 
     }
