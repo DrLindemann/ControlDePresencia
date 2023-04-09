63ab2709b44bebb974fce43ca92dbec5236d23ed
DrLindemann <69mmc69@gmail.com>
2023-04-10 00:26:28 +0200

    añadidos listas y algunas variables en Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
+        boolean runApp = true;
+        Trabajador trabajador = new Trabajador();
+        List<Trabajador> listaEntradas = new ArrayList<>();
+        List<Trabajador> listaSalidas = new ArrayList<>();
+
 
     }
 }
